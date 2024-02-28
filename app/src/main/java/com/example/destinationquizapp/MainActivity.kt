695a70ecf676347aa559820756441473eb698763
groovinflannel <groovinflannel@gmail.com>
2024-02-28 12:40:22 -0500

    practice using simple butotn

--- app/src/main/java/com/example/destinationquizapp/MainActivity.kt
+++ app/src/main/java/com/example/destinationquizapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.destinationquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.new_layout)
+
+        button = findViewById(R.id.button2)
+        button.text = "Login Button"
     }
 }
