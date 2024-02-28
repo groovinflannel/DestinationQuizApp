5b1c17d7c50a011e3ca960b622b46a20864e0837
groovinflannel <groovinflannel@gmail.com>
2024-02-28 13:47:17 -0500

    added listent and lambda function to myButton for clicks; 'Show' button displays text 'Hello, Sesu' on click

--- app/src/main/java/com/example/destinationquizapp/MainActivity.kt
+++ app/src/main/java/com/example/destinationquizapp/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.destinationquizapp
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var button: Button
-
+    private lateinit var myButton: Button
+    private lateinit var myTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.new_layout)
+        setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.button2)
-        button.text = getString(R.string.button_text)
+        myButton = findViewById(R.id.myButton)
+        myTextView = findViewById(R.id.myTextView)
+
+        myButton.text = getString(R.string.button_text)
+
+        myButton.setOnClickListener {
+            myTextView.visibility = View.VISIBLE
+            myTextView.text = getString(R.string.text_view_text)
+        }
+
     }
 }
